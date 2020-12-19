bbad02bb6823bd16c61012b85551f8b99a0387be
Kai Peng Li <oopanpan@gmail.com>
2020-12-19 07:39:31 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,7 +15,7 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  new_array = assembled_aol.sorted
+  new_array = assembled_aol.sort
   new_array
 end
 
